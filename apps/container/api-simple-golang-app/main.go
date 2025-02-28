0f955fbeb1d6e69df5139ec60f401ddc1ed060f6
Quincy Cheng <quincy.cheng@gmail.com>
2025-02-28 12:27:21 +0800

    updated api-simple-golang-app for custom secret path from Conjur

--- apps/container/api-simple-golang-app/main.go
+++ apps/container/api-simple-golang-app/main.go
@@ -1,38 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/cyberark/conjur-api-go/conjurapi"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/cyberark/conjur-api-go/conjurapi"
 )
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-    // Fetch secrets from Conjur using authn-JWT
-    variableIdentifier := "data/vault/POV/Database-MySQL-database.pov.example.com-admin/password"
-    config, err := conjurapi.LoadConfig()
-    if err != nil {
-	fmt.Println("ERROR")
-        panic(err)
-    }
-	
-    conjur, err := conjurapi.NewClientFromEnvironment(config)
-    if err != nil {
-	fmt.Println("ERROR 2")
-        panic(err)
-    }
-	
-    secretValue, err := conjur.RetrieveSecret(variableIdentifier)
-    if err != nil {
-	fmt.Println("ERROR 3")
-        panic(err)
-    }
+	// Get the environment variable
+	variableIdentifier := os.Getenv("CONJUR_SECRET_PATH")
 
-    fmt.Fprintf(w, "The Secret is: " + string(secretValue) )
+	// Check if the variable is empty
+	if variableIdentifier == "" {
+		// Set default value
+		variableIdentifier = "data/vault/POV/Database-MySQL-database.pov.example.com-admin/password"
+	}
+
+	config, err := conjurapi.LoadConfig()
+	if err != nil {
+		fmt.Println("ERROR")
+		panic(err)
+	}
+
+	conjur, err := conjurapi.NewClientFromEnvironment(config)
+	if err != nil {
+		fmt.Println("ERROR 2")
+		panic(err)
+	}
+
+	secretValue, err := conjur.RetrieveSecret(variableIdentifier)
+	if err != nil {
+		fmt.Println("ERROR 3")
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "The Secret is: "+string(secretValue))
 }
-
